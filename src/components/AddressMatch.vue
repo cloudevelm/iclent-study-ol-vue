745728b601779ba881fddbf7fc5d467139142f27
cloudevelm <18687320965@163.com>
2022-08-29 15:35:25 +0800

    增加地名地址查询

---
+++ src/components/AddressMatch.vue
@@ -0,0 +1,26 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: "AddressMatch",
+  mounted() {
+    const addressMatchService = new window.ol.supermap.AddressMatchService('https://iserver.supermap.io/iserver/services/addressmatch-Address/restjsr/v1/address')
+    const param = new window.SuperMap.GeoCodingParameter({
+      address: '超图软件'
+    })
+    console.log('------------------------------------地址匹配------------------------------------')
+    addressMatchService.code(param, e => {
+      console.log(e)
+    })
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
